66edb0ed22125dcbdbde5bf66b473317ce3dcd8b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-12 15:00:11 +0100

    Créer un module Admin

---
+++ src/app/admin/article-new/article-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-new.component.html',
+  styleUrl: './article-new.component.css'
+})
+export class ArticleNewComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule)}
+    { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule)},
+    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
